4589af07ea9afbe9636544356a26f82b396c18ac
tanavposwal <tanavposwal939@gmail.com>
2024-11-18 11:30:29 +0530

    ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,19 @@ function App() {
   const [asciiImageUrl, setAsciiImageUrl] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleImageChange = (event: any) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result as any);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   const uploadImage = async () => {
     const formData = new FormData();
 
@@ -38,43 +51,80 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full min-h-screen">
-      <h1 className="text-6xl font-black text-gray-700 mb-8 font-serif text-center">
-        Retro Image <br /> Converter
-      </h1>
-      <div className="border rounded-xl overflow-hidden p-6">
-      <form
-        id="uploadForm"
-        className="flex items-center justify-center font-serif"
-      >
-        <input
-          type="file"
-          id="fileInput"
-          name="image"
-          title="only png/jpg/jpeg"
-          accept="image/png, image/jpg, image/jpeg"
-          ref={fileInput}
-          className="file:bg-white file:border-nonde file:text-xl file:font-bold file:rounded-xl file:p-6 file:cursor-pointer file:hover:bg-gray-100 file:transition-colors "
-        />
-        <button
-          type="button"
-          onClick={uploadImage}
-          className="bg-white border border-blue-600 hover:bg-blue-600 text-blue-600 hover:text-white font-bold py-6 px-6 transition-colors rounded-xl active text-xl"
-        >
-          Convert
-        </button>
-      </form>
-
-      {(loading || asciiImageUrl) && (
-        <div className="w-full min-h-48 flex items-center justify-center mt-6">
-          {loading && <img src={Loading} className="w-8" />}
-          {(asciiImageUrl && !loading) && <img src={asciiImageUrl} alt="ASCII Art" className="max-h-72" />}
-        </div>
-      )}
+    <div className="flex flex-col items-center w-full bg-yellow-100 min-h-screen">
+      <div className="border-4 border-black px-10 py-5 bg-green-200 mt-10 mb-5 -skew-x-3 -skew-y-1 shadow-[6px_7px_0px_black]">
+        <h1 className="text-3xl font-black text-black text-center">
+          Retro Image <br /> Converter
+        </h1>
       </div>
-
-      <div className="mt-8 text-gray-500">
-        AD
+      <div className="border-4 border-black px-16 py-12 bg-blue-200 mt-8 mb-5 shadow-[0px_0px_0px_black]">
+        <form
+          id="uploadForm"
+          className="flex flex-col items-center justify-center"
+        >
+          {/* images */}
+          <div className="flex gap-3">
+            <div className="min-w-48 max-w-56 min-h-56 max-h-64 flex items-center justify-center border-4 border-black relative group overflow-hidden bg-white">
+              {!selectedImage && <span>select one</span>}
+              {selectedImage && (
+                <div>
+                  <div className="absolute -bottom-8 opacity-0 blur-xl left-3 px-2 py-1 font-mono bg-black text-white group-hover:bottom-3 group-hover:opacity-90 group-hover:blur-0 transition-all">
+                    Old
+                  </div>
+                  <img src={selectedImage} />
+                </div>
+              )}
+            </div>
+            <div className="min-w-48 max-w-56 min-h-56 max-h-64 flex items-center justify-center border-4 border-black relative group overflow-hidden bg-white">
+              {!selectedImage && <span>select one first</span>}
+              {(loading || asciiImageUrl) && (
+                <div>
+                  {loading && <img src={Loading} className="w-8" />}
+                  {asciiImageUrl && !loading && (
+                    <div>
+                      <div className="absolute -bottom-8 opacity-0 blur-xl left-3 px-2 py-1 font-mono bg-black text-white group-hover:bottom-3 group-hover:opacity-90 group-hover:blur-0 transition-all">
+                        New
+                      </div>
+                      <img
+                        src={asciiImageUrl}
+                        alt="ASCII Art"
+                        className="max-h-72"
+                      />
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          </div>
+          {/* buttons */}
+          <div className="flex gap-6 mt-5">
+            <div>
+              <label
+                htmlFor="fileInput"
+                className="cursor-pointer bg-purple-200 px-4 py-2 border-4 border-black shadow-[3px_4px_0px_black] hover:bg-purple-300 hover:shadow-[2px_3px_0px_black] text-lg transition-all duration-300 font-bold block"
+              >
+                Choose an Image
+              </label>
+              <input
+                type="file"
+                id="fileInput"
+                name="image"
+                title="only png/jpg/jpeg"
+                accept="image/png, image/jpg, image/jpeg"
+                ref={fileInput}
+                onChange={handleImageChange}
+                className="hidden"
+              />
+            </div>
+            <button
+              type="button"
+              onClick={uploadImage}
+              className="cursor-pointer bg-red-200 px-4 py-2 border-4 border-black shadow-[3px_4px_0px_black] hover:bg-red-300 hover:shadow-[2px_3px_0px_black] text-lg transition-all duration-300 font-bold"
+            >
+              Convert
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
